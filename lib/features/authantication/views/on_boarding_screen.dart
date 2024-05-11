a7d2a495595ac0f203a6e1e2b94af582260b0b44
farsemac1 <farsemac1@gmail.com>
2024-05-11 19:57:28 +0000

    skip button

--- lib/features/authantication/views/on_boarding_screen.dart
+++ lib/features/authantication/views/on_boarding_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:myapp/core/common/widgets/white_space.dart';
 import 'package:myapp/core/res/image_res.dart';
 
