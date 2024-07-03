b2660489c0cb2b08b1b356d5328240887082b4f6
farsemac1 <farsemac1@gmail.com>
2024-07-03 10:02:50 +0000

    sign in page

--- lib/features/authantication/views/sign_in_screen.dart
+++ lib/features/authantication/views/sign_in_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/core/common/widgets/white_space.dart';
+import 'package:myapp/core/res/image_res.dart';
 
 class SignInScreen extends StatelessWidget {
   const SignInScreen({super.key});
@@ -7,16 +9,53 @@ class SignInScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 39, 39, 39),
-      body: Center(
-        child: Text("Sign In",
-        style: TextStyle(
-          fontSize: 30,
-          color: Colors.white,
-          fontWeight: FontWeight.w500, 
-        ),)
-        ,
-      
-    ),
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.symmetric(horizontal:30),
+          children: [
+            Image.asset(Imageres.todo),
+            WhiteSpace(height: 20,),
+            Text("Log in With Mobile Number",
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:3532102179.
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.white,
+              fontWeight: FontWeight.w500,
+            ),
+            ),
+            WhiteSpace(height: 20,),
+            TextField(
+              
+              style: TextStyle(
+                
+                fontSize: 20,
+                color: Colors.white,
+                fontWeight: FontWeight.w500,
+              ),
+              decoration: InputDecoration(
+               border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20),
+                borderSide: BorderSide.none,
+               ),
+                filled: true,
+              
+                hintText: "Mobile Number",
+                hintStyle: TextStyle(
+                  fontSize: 20,
+                  
+                  color: Color.fromARGB(255, 124, 124, 124),
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+
+
+
+          ],
+
+        ) ,
+      ),
     );
   }
 }
